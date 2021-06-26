7a0b9599aac19806d20ed24314b236493373cc97
darkobas <darkobas@gmail.com>
2021-06-26 14:36:20 +0530

    sm8250-common: DeviceSettings: Switch back to old DC-Dimming implemention
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- DeviceSettings/src/org/havoc/device/DeviceSettings/DeviceSettings.java
+++ DeviceSettings/src/org/havoc/device/DeviceSettings/DeviceSettings.java
@@ -54,6 +54,7 @@ import org.havoc.device.DeviceSettings.FPSInfoService;
 public class DeviceSettings extends PreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
+    public static final String KEY_DC_SWITCH = "dc";
     public static final String KEY_HBM_SWITCH = "hbm";
     public static final String KEY_AUTO_HBM_SWITCH = "auto_hbm";
     public static final String KEY_AUTO_HBM_THRESHOLD = "auto_hbm_threshold";
@@ -62,6 +63,7 @@ public class DeviceSettings extends PreferenceFragment
     public static final String KEY_SETTINGS_PREFIX = "device_setting_";
 
     private static SwitchPreference mFpsInfo;
+    private static TwoStatePreference mDCModeSwitch;
     private static TwoStatePreference mHBMModeSwitch;
     private static TwoStatePreference mAutoHBMSwitch;
 
@@ -86,6 +88,11 @@ public class DeviceSettings extends PreferenceFragment
         if (mVibratorStrength == null || !VibratorStrengthPreference.isSupported()) {
             getPreferenceScreen().removePreference((Preference) findPreference("vibrator"));
         }
+
+        mDCModeSwitch = (TwoStatePreference) findPreference(KEY_DC_SWITCH);
+        mDCModeSwitch.setEnabled(DCModeSwitch.isSupported());
+        mDCModeSwitch.setChecked(DCModeSwitch.isCurrentlyEnabled(this.getContext()));
+        mDCModeSwitch.setOnPreferenceChangeListener(new DCModeSwitch());
 
         mHBMModeSwitch = (TwoStatePreference) findPreference(KEY_HBM_SWITCH);
         mHBMModeSwitch.setEnabled(HBMModeSwitch.isSupported());
