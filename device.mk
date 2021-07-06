137632757d4d977f2d9978b03520ff7db17cd186
YumeMichi <do4suki@gmail.com>
2021-07-06 18:24:37 +0800

    lemonkebab: Add bt-mac-generator.
    Since our RIL isn't dumping NV items on boot, we are always falling back
    to random bluetooth MAC address. This change makes it at least stable...
    
    Co-authored-by: codeworkx <daniel.hillenbrand@codeworkx.de>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: I475f1a2fc33a17dda4f23aa800bf62e36b58a172

--- device.mk
+++ device.mk
@@ -144,6 +144,7 @@ PRODUCT_PACKAGES += \
   android.hardware.bluetooth@1.0.vendor \
   audio.bluetooth.default \
   android.hardware.bluetooth@1.0-service-qti \
+  bt-mac-generator \
   vendor.qti.hardware.bluetooth_audio@2.0.vendor \
   vendor.qti.hardware.btconfigstore@1.0.vendor \
   vendor.qti.hardware.btconfigstore@1.1 \
