a8baff0a4ba546ce2fd191eceeee6908ad1dc9b6
chandu078 <chandudyavanapelli03@gmail.com>
2021-09-09 03:01:20 +0000

    sm8250-common: Update blobs from LA.UM.9.12.r1-11500-SMxx50.0
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- lemonkebab-vendor.mk
+++ lemonkebab-vendor.mk
@@ -1499,7 +1499,6 @@ PRODUCT_PACKAGES += \
     CneApp \
     IWlanService \
     TimeService \
-    com.oneplus.gpudrivers.kona.api30 \
     com.qualcomm.qti.gpudrivers.kona.api30 \
     PowerOffAlarm \
     HotwordEnrollmentOKGoogleHEXAGON \
