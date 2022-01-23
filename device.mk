62675befb4238dffcad565f287bd2e815b269722
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-01-23 22:45:11 +0530

    lemonkebab: Added perf configs from stock
    - Why not support PerformanceMode
    - OnePlus is Ghey

--- device.mk
+++ device.mk
@@ -356,6 +356,15 @@ TARGET_COMMON_QTI_COMPONENTS := \
     usb \
     wfd
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
+    $(LOCAL_PATH)/configs/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
+    $(LOCAL_PATH)/configs/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml \
+    $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+
 # RIL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full
